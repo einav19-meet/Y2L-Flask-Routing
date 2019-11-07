c210b3c654ec9dcbfe426e335ccad5b195001b1f
einav19-meet <einav19@meet.mit.edu>
2019-11-07 13:54:17 +0200

    remove

--- app.py
+++ app.py
@@ -7,23 +7,6 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 ##### Code here ######
 
-@app.route('/home')
-def go_home():
-	return render_template("hello.html")
-
-@app.route('/store')
-def go_store():
-	return render_template("store.html")
-
-	
-@app.route('/cart')
-def my_cart():
-	return render_template("cart.html")
-
-@app.route('/about')
-def about():
-	return render_template("about.html")
-
 #####################
 
 
